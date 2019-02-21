3dba1827b85d31c7a275fc8a9458ba9e306f594f
Oleg Boloban <o.boloban@geniusee.com>
2019-02-21 14:56:04 +0200

    header

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,33 @@
-console.log('\'Allo \'Allo!');
+// mobile header button
 
-// Uncomment to enable Bootstrap tooltips
-// https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
-// $(function () { $('[data-toggle="tooltip"]').tooltip(); });
+let mobileButton = document.querySelector('.mobile-button');
+let menu = document.querySelector('.nav');
+let showMenu = false;
 
-// Uncomment to enable Bootstrap popovers
-// https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
-// $(function () { $('[data-toggle="popover"]').popover(); });
+mobileButton.addEventListener('click', ()=>{
+  if(!showMenu){
+    mobileButton.classList.add('active');
+    menu.classList.add('active');
+    showMenu=true;
+  }
+  else{
+    mobileButton.classList.remove('active');
+    menu.classList.remove('active');
+    showMenu=false;
+  }
+});
+
+// active menu link
+
+let bodyClass = document.querySelector('body').getAttribute('class');
+let links = document.querySelectorAll('nav ul li a');
+
+links.forEach(link=>{
+  let className = link.getAttribute('class');
+  link.classList.remove('active');
+  if(className === bodyClass){
+    link.classList.add('active')
+  }
+});
+
+
